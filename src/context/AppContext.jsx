22d21aaf31bd836fa59d1824ab854eae4d10d2f5
Krishna Acondy <kc.acondy@outlook.com>
2020-07-09 07:12:31 +0100

    fix(imports): fix SASjs import

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect } from "react";
-import SASjs from "sasjs";
+import SASjs from "@sasjs/adapter";
 import { useCallback } from "react";
 
 const defaultConfig = {
